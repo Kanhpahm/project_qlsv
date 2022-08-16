3c919808d26a8ecf21ff4cb9e9411349680c7b34
Hieu Nguyen <hieunguyen250102@gmail.com>
2022-08-16 14:52:03 +0700

    client

---
+++ resources/views/client/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.client.master')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            <h1 class="mt-5">Online Examination System</h1>
+            <p class="lead">Simple MCQ Based Online Examination System!</p>
+            <img src="{{asset('image/bgtest.png')}}" width="130px;" />
+        </div>
+    </div>
+</div>
+@endsection
